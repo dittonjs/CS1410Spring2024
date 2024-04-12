019e8ff4411240a66923fcd6f219fcf72be03a3f
Joseph Ditton <jditton.atomic@gmail.com>
2024-04-12 08:38:03 -0600

    class from wednesday

--- BinarySearchTree/src/BinarySearchTree.java
+++ BinarySearchTree/src/BinarySearchTree.java
@@ -1,3 +1,8 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree {
     private class TreeNode {
         int data;
@@ -36,29 +41,95 @@ public class BinarySearchTree {
         }
     }
 
-    public void display() {
-        display(root, "");
+    public void display(String message) {
+        System.out.println(message);
+        displayInOrder(root);
     }
 
-    private void display(TreeNode node, String prefix) {
+    private void displayInOrder(TreeNode node) {
+        if (node == null) return;
+        displayInOrder(node.left);
+        System.out.println(node.data);
+        displayInOrder(node.right);
+    }
+
+    public void displayPreOrder() {
+        displayPreOrder(root);
+    }
+
+    private void displayPreOrder(TreeNode node) {
         if (node == null) return;
 
-        display(node.right, prefix + "    ");
+        System.out.println(node.data);
+        displayPreOrder(node.left);
+        displayPreOrder(node.right);
+    }
+
+    public void displayPostOrder() {
+        displayPostOrder(root);
+    }
+
+    private void displayPostOrder(TreeNode node) {
+        if (node == null) return;
+
+        displayPostOrder(node.left);
+        displayPostOrder(node.right);
+        System.out.println(node.data);
+    }
+
+    public BinarySearchTree copy() {
+        BinarySearchTree newTree = new BinarySearchTree();
+        copy(root, newTree);
+        return newTree;
+    }
+
+    private void copy(TreeNode node, BinarySearchTree newTree) {
+        if (node == null) return;
+
+        newTree.insert(node.data);
+        copy(node.right, newTree);
+        copy(node.left, newTree);
+    }
+
+    public void displayBreadthFirst() {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+            System.out.println(node.data);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+
+
+    public void displayTreeStructure() {
+        displayTreeStructure(root, "");
+    }
+
+    private void displayTreeStructure(TreeNode node, String prefix) {
+        if (node == null) return;
+
+        displayTreeStructure(node.right, prefix + "    ");
         System.out.println(prefix + node.data);
-        display(node.left, prefix + "    ");
+        displayTreeStructure(node.left, prefix + "    ");
     }
 
-    public boolean find(int data) {
-        return find(root, data);
+    public boolean search(int data) {
+        return search(root, data);
     }
 
-    private boolean find(TreeNode node, int data) {
+    private boolean search(TreeNode node, int data) {
         if (node == null) return false;
         if (node.data == data) return true;
         if (data < node.data) { // value must be on the left
-            return find(node.left, data);
+            return search(node.left, data);
         } else {
-            return find(node.right, data);
+            return search(node.right, data);
         }
     }
 
@@ -66,7 +137,7 @@ public class BinarySearchTree {
         TreeNode parent = null;
         TreeNode node = root;
 
-        // find the node to attach the new node to
+        // find the node you are deleting
         boolean done = false;
         while(!done && node != null) {
             if(data < node.data) {// go to left
@@ -83,6 +154,9 @@ public class BinarySearchTree {
         if (node == null) return;
 
         if (node.left == null) {
+            // where there is no left child we can do something
+            // similar to what we do with a linked list - link the parent
+            // node to the right child. Remember to handle the root!
             if (node == root) {
                 root = node.right;
             } else if (node == parent.left) {
@@ -90,8 +164,19 @@ public class BinarySearchTree {
             } else {
                 parent.right = node.right;
             }
-        } else { // left node == less easy
+        } else {
+            /* If there is a left child, we have to so something more complex.
+             1. Find the right-most node of the left sub-tree
+             2. Keep track of the parent of the right most node
+             3. When you find the right most node replace the value in the node
+                replace the value in the node you are deleting with the value in
+                the right-most node.
+             4. Attach the left node of the right-most node to either
+                the left or the right of the right-most's parent, depending on
+                which side the right most is found on.
+             */
            TreeNode parentOfRightMost = node;
+
            TreeNode rightMost = node.left;
            while(rightMost.right != null) {
                parentOfRightMost = rightMost;
--- BinarySearchTree/src/Main.java
+++ BinarySearchTree/src/Main.java
@@ -1,44 +1,59 @@
+import java.util.StringJoiner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(args[0]);
         BinarySearchTree tree = new BinarySearchTree();
         tree.insert(6);
-        tree.insert(4);
         tree.insert(9);
+        tree.insert(4);
         tree.insert(2);
         tree.insert(5);
         tree.insert(7);
         tree.insert(11);
         tree.insert(12);
 
-        tree.display();
+        System.out.println("---- original tree ----");
+        tree.displayTreeStructure();
+        tree.displayBreadthFirst();
 
-        tree.remove(11);
-
-        System.out.println();
-        tree.display();
-
-        tree.remove(4);
-
-        System.out.println();
-        tree.display();
-
-        tree.remove(6);
-
-        System.out.println();
-        tree.display();
-
-        tree.remove(2);
-
-        System.out.println();
-        tree.display();
-
-        tree.remove(5);
-
-        System.out.println();
-        tree.display();
-
-        System.out.println(tree.find(3));
-        System.out.println(tree.find(11));
+        BinarySearchTree copy = tree.copy();
+        System.out.println("---- copy tree ----");
+        copy.displayTreeStructure();
+        tree.display("--- displaying in order ---");
+        System.out.println("--- displaying pre-order ---");
+        tree.displayPreOrder();
+        System.out.println("--- displaying post-order ---");
+        tree.displayPostOrder();
+        // TESTING REMOVAL
+//        tree.displayTreeStructure();
+//
+//        tree.remove(11);
+//
+//        System.out.println();
+//        tree.displayTreeStructure();
+//
+//        tree.remove(4);
+//
+//        System.out.println();
+//        tree.displayTreeStructure();
+//
+//        tree.remove(6);
+//
+//        System.out.println();
+//        tree.displayTreeStructure();
+//
+//        tree.remove(2);
+//
+//        System.out.println();
+//        tree.displayTreeStructure();
+//
+//        tree.remove(5);
+//
+//        System.out.println();
+//        tree.displayTreeStructure();
+//
+//        System.out.println(tree.search(3));
+//        System.out.println(tree.search(11));
     }
 }
